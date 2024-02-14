d3df59a5aa92ac8e773f0695fd1fbe46cdfb576b
m-sarful <isarful823@gmail.com>
2024-02-14 12:36:13 +0600

    adding display hide and show

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,13 @@
+// function play(){
+//    const homeScreen = document.getElementById('home');
+//    homeScreen.classList.add('hidden');
+
+//    const playgroundScreen = document.getElementById('play-ground');
+//    playgroundScreen.classList.remove('hidden')
+// }
+
+
+function play(){
+    hideElementById('home');
+    showElementById('play-ground');
+}
